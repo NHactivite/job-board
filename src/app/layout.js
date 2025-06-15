a3252df3168e73ae5d6a790a07de3eb5a72e3df0
NIKHIL HAZARIKA <nikhilhazarika9@gmail.com>
2025-06-15 13:42:39 +0530

    makeing pwa app

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
     <ClerkProvider>
       <html lang="en">
       <link rel="manifest" href="/manifest.json" />
-     <link rel="apple-touch-icon" href="/icons/icon192x192.png" />
+     <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
     <meta name="theme-color" content="#000000" />
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
       <ClerkLoading>
